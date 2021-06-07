4a0a43e9ba13199fddba9764c0e523a4a7988f20
gemmamerkaj <84473823+gemmamerkaj@users.noreply.github.com>
2021-06-07 12:59:33 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -47,8 +47,33 @@
 		header("Content-Type: application/json");
 		//i parametri sono cosa voglio mandare indietro al mio utente
 		$parameters = array('chat_id' => $chatId, "text" => $text);
+		if($text == "ciao"){
+			$risp ="Benvenuto dulla pagina del Bot del IIS8Marzo";
+			$parameters = array('chat_id' => $chatId, "text" => $risp);
+		}
+		if($text == "data"){
+			$risp = "La data odierna è: ".date("d.m.y");
+			$parameters = array('chat_id' => $chatId, "text" => $risp);
+		}
+		if($text == "foto"){
+			$foto[0] = "foto.jpg";
+			$foto[1] = "foto1.jpg";
 		
+			$i= rand(0,1);
 		
+			sendFoto($chatId, $foto[$i], false, "descrizione Foto", $api)
+		}
+
+		if($text == "barze"){
+    			//Salvo all'interno del vettore $barze 2 bazelette in posizioni differenti
+    			$barze[0]="Che cos'è una zebra? Un cavallo evaso dal cxarcere!";
+      			$barze[1]="Qual'è il colmo per un truffatore?Fare un buco nell'acqua!";
+      			$barze[2]="Chi la fa la vende, chi la compra non la USA, chi la usa non la vende, cosa è??? La tomba."
+               		$barze[3]="Qual'è il colmo per un giardiniere?Piantare la fidandata!";
+    			//genero un numero random tra 0 e 2 e lo salvo nella variabile $i 
+      			$i=rand(0,3);
+    		}
+
 		
 		
 		//aggiungo il comando di invio
